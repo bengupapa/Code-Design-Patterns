7c4060818a1840f9db31e24018a565a405235afe
papa.bengu@touchlab.co.za <b1e2n3g4u5>
2015-07-21 08:42:36 +0200

    adde sigleton class library

---
+++ DesignPatterns.Singleton/BaseManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public class BaseManager
+    {
+        private static BaseManager InitialInstance = null;
+
+        public List<IPayer> Players;
+
+        private BaseManager()
+        {
+            Players = new List<IPayer>()
+            {
+                new SoccerPlayer(),
+                new TennisPlayer(),
+                new VolleyBallPlayer(),
+            };
+
+            Players.ForEach(p => p.SetRemuneration());
+        }
+
+        public static BaseManager GetInstance()
+        {
+            if(InitialInstance == null)
+                InitialInstance = new BaseManager();
+
+            return InitialInstance;
+        }
+
+    }
+}
---
+++ DesignPatterns.Singleton/IPayer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public interface IPayer
+    {
+        string PlayerName { get; }
+        int NumberOfGamesPlayed { get; }
+        decimal Remuneration { get;}
+
+        int GetGamesPlayed();
+        void SetRemuneration(decimal salary = 7000);
+    }
+
+    public class SoccerPlayer : IPayer
+    {
+        private decimal Salary;
+
+        public string PlayerName
+        {
+            get { return "Papa Bengu"; }
+        }
+
+        public int NumberOfGamesPlayed
+        {
+            get { return 7; }
+        }
+
+        public int GetGamesPlayed()
+        {
+            return this.NumberOfGamesPlayed;
+        }
+
+        public void SetRemuneration(decimal salary = 23470)
+        {
+            this.Salary = salary;
+        }
+
+        public decimal Remuneration
+        {
+            get { return this.Salary; }
+        }
+    }
+
+    public class TennisPlayer : IPayer
+    {
+
+        private decimal Salary;
+
+        public string PlayerName
+        {
+            get { return "Madillo Bengu"; }
+        }
+
+        public int NumberOfGamesPlayed
+        {
+            get { return 10; }
+        }
+
+        public decimal Remuneration
+        {
+            get { return this.Salary; }
+        }
+
+        public int GetGamesPlayed()
+        {
+            return this.NumberOfGamesPlayed;
+        }
+
+        public void SetRemuneration(decimal salary = 67560)
+        {
+            this.Salary = salary;
+        }
+    }
+
+    public class VolleyBallPlayer : IPayer
+    {
+        private decimal Salary;
+
+        public string PlayerName
+        {
+            get { return "Dikeledi Ngqwemla"; }
+        }
+
+        public int NumberOfGamesPlayed
+        {
+            get { return 20; }
+        }
+
+        public decimal Remuneration
+        {
+            get { return this.Salary; }
+        }
+
+        public int GetGamesPlayed()
+        {
+            return NumberOfGamesPlayed;
+        }
+
+        public void SetRemuneration(decimal salary = 5600)
+        {
+            this.Salary = salary;
+        }
+    }
+}
